f3a39e622641ef7c0a00806ebbf8c00ba0b6c4a2
Tyler <tylergross28@gmail.com>
2018-05-23 16:20:00 -0500

    Setting up to send forms to firestore

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,31 @@
-// const log = () => {
-//   console.log('This file is doing stuff.')
-// }
+const firebase = require("firebase");
+require("firebase/firestore");
 
-// const submit = document.getElementById('scheduleTour')
-// submit.addEventListener('click', log)
+const db = firebase.firestore();
+const $ = (id) => document.getElementById(id)
+
+const onSave = () => {
+    const name = $('name')
+    const email = $('email3')
+    const age = $('child-age')
+    const date = $('date')
+    const time = $('time3')
+    const formData = {
+        name: name.value,
+        email: email.value,
+        childAge: age.value,
+        date: date.value,
+        time: time.value
+    }
+    db.collection('tours').add(formData)
+    .then(function(docRef) {
+        console.log(`Database write complete.`);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+    
+  }
+
+const submit = $('scheduleTour')
+submit.addEventListener('click', onSave)
