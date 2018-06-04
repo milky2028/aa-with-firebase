cf0cf5cd6d1a579faf1a5f46af482fc22f67f93d
Tyler <tylergross28@gmail.com>
2018-06-04 00:51:07 -0500

    still trying to get the db working

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,23 @@
+const config = {
+    apiKey: "AIzaSyDkRY5dlk-aBvDYpDkyvLLC8nPDxXt2C60",
+    authDomain: "adventure-academ-1501025774496.firebaseapp.com",
+    databaseURL: "https://adventure-academ-1501025774496.firebaseio.com",
+    projectId: "adventure-academ-1501025774496",
+    storageBucket: "adventure-academ-1501025774496.appspot.com",
+    messagingSenderId: "784182344237"
+};
+firebase.initializeApp(config);
+
 const settings = { timestampsInSnapshots: true };
 const db = firebase.firestore();
 db.settings(settings);
 
 const onSave = () => {
-    const name = document.querySelector('#name')
-    const email = document.querySelector('#email3')
-    const age = document.querySelector('#child-age')
-    const tourDate = document.querySelector('#date')
-    const tourTime = document.querySelector('#time3')
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email3');
+    const age = document.querySelector('#child-age');
+    const tourDate = document.querySelector('#date');
+    const tourTime = document.querySelector('#time3');
     const formData = {
         name: name.value,
         email: email.value,
@@ -15,13 +25,20 @@ const onSave = () => {
         tourDate: tourDate.value,
         tourTime: tourTime.value,
         submitTime: Date.now()
-    }
-    db.collection('tours').add(formData).then(docRef => {
-        console.log(`Database write complete.`);
-    }).catch(error => {
-        console.error("Error adding document: ", error);
-    })
+    };
+    
+    // db.collection('tours').get().then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //         console.log(doc)
+    //         console.log(doc.data())
+    //     })
+    // })
+    // db.collection('tours').add(formData).then(docRef => {
+    //     console.log('Database write complete:', docRef.id);
+    // }).catch(error => {
+    //     console.error('Error adding document: ', error);
+    // })
   }
 
-const submit = document.querySelector('#scheduleTour')
-submit.addEventListener('click', onSave)
+const submit = document.querySelector('#scheduleTour');
+submit.addEventListener('click', onSave);
