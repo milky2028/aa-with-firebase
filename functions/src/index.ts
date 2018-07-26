5db39751edb5c207a4225db8188e39378e985054
Tyler <tylergross28@gmail.com>
2018-07-26 17:46:34 -0500

    sendgrid mail functions (not working)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as sgMail from '@sendgrid/mail';
+import { SENDGRID_API_KEY } from '../sendgridEnv'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// sgMail.setApiKey(SENDGRID_API_KEY);
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+const msg = {
+    to: 'tylergross28@gmail.com',
+    from: 'support@adventureacademyweb.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+export const sendMail = functions.https.onRequest((request, response) => {
+  response.send(msg);
+  // sgMail.send(msg).then(() => {
+  //   response.send('It worked!');
+  // }).catch(error => {
+  //     console.error('It did not work :[');
+  // });
 });
