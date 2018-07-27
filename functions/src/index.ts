9ecee4fd38f742bb3507e7c389093da2c98fe5ac
Tyler <tylergross28@gmail.com>
2018-07-26 20:47:55 -0500

    Hopefully working form submission

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,38 +3,19 @@ import * as sgMail from '@sendgrid/mail';
 import { SENDGRID_API_KEY } from './sendgridEnv'
 
 sgMail.setApiKey(SENDGRID_API_KEY);
-const msg = {
-  to: 'tylergross28@gmail.com',
-  from: 'support@adventureacademyweb.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
+export const tourFormSubmission = functions.firestore.document('tours/{tourId}').onCreate((snapshot, context) => {
+  const submission = snapshot.data();
 
-export const mailTest = functions.https.onRequest((request, response) => {
-  sgMail.send(msg).then(() => {
+  const msg = {
+    to: submission.email,
+    from: 'support@adventureacademyweb.com',
+    subject: 'Hey Mom! Someone Submitted a New Tour Form',
+    html: `<div><h1 style="font-size: 20px">Here are the details about the tour:</h1><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Name: ${submission.name}</p><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Kid\'s Age: ${submission.childAge}</p><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Tour Date: ${submission.tourDate}</p><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Tour Time: ${submission.tourTime}</p><p style="font-size: 18px; font-family: Lato">I love you mom! You\'re the greatest.</p><p style="font-size: 18px; font-family: Lato; font-weight:bold">Tyler</p></div>`,
+  };
+
+  return sgMail.send(msg).then(() => {
     console.log('It worked!');
   }).catch(error => {
     console.error('It did not work :[');
   });
-  response.send('This is me sending a response.');
-});
-
-export const formSubmission = functions.firestore.document('tours').onCreate((snapshot, context) => {
-  const submission = snapshot.data();
-  console.log(submission);
-
-  // const msg = {
-  //   to: 'tylergross28@gmail.com',
-  //   from: 'support@adventureacademyweb.com',
-  //   subject: 'Sending with SendGrid is Fun',
-  //   text: 'and easy to do anywhere, even with Node.js',
-  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  // };
-
-  // return sgMail.send(msg).then(() => {
-  //   console.log('It worked!');
-  // }).catch(error => {
-  //   console.error('It did not work :[');
-  // });
 });
