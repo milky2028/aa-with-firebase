810556932d3fe99e8b35c9cca4034f59d5513a98
Tyler <tylergross28@gmail.com>
2018-07-26 20:58:48 -0500

    font updates and proper addresses

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,14 +6,14 @@ sgMail.setApiKey(SENDGRID_API_KEY);
 export const tourFormSubmission = functions.firestore.document('tours/{tourId}').onCreate((snapshot, context) => {
   const submission = snapshot.data();
 
-  const msg = {
-    to: submission.email,
+  const msgToMom = {
+    to: 'tylergross28@gmail.com, tgross1@me.com',
     from: 'support@adventureacademyweb.com',
     subject: 'Hey Mom! Someone Submitted a New Tour Form',
-    html: `<div><h1 style="font-size: 20px">Here are the details about the tour:</h1><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Name: ${submission.name}</p><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Kid\'s Age: ${submission.childAge}</p><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Tour Date: ${submission.tourDate}</p><p style="font-size: 18px; margin: 3px 10px; font-family: Lato">Tour Time: ${submission.tourTime}</p><p style="font-size: 18px; font-family: Lato">I love you mom! You\'re the greatest.</p><p style="font-size: 18px; font-family: Lato; font-weight:bold">Tyler</p></div>`,
+    html: `<div><h1 style="font-size: 20px">Here are the details about the tour:</h1><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Name: ${submission.name}</p><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Kid\'s Age: ${submission.childAge}</p><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Date: ${submission.tourDate}</p><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Time: ${submission.tourTime}</p><p style="font-size: 18px; font-family: sans-serif">I love you mom! You\'re the greatest.</p><p style="font-size: 18px; font-family: sans-serif; font-weight:bold">Tyler</p></div>`,
   };
 
-  return sgMail.send(msg).then(() => {
+  return sgMail.send(msgToMom).then(() => {
     console.log('It worked!');
   }).catch(error => {
     console.error('It did not work :[');
