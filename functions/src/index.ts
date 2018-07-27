af4aa60be427de11a83d5291b737893c6da3d573
Tyler <tylergross28@gmail.com>
2018-07-26 21:03:19 -0500

    email look updates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,7 @@ export const tourFormSubmission = functions.firestore.document('tours/{tourId}')
     to: 'tylergross28@gmail.com, tgross1@me.com',
     from: 'support@adventureacademyweb.com',
     subject: 'Hey Mom! Someone Submitted a New Tour Form',
-    html: `<div><h1 style="font-size: 20px">Here are the details about the tour:</h1><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Name: ${submission.name}</p><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Kid\'s Age: ${submission.childAge}</p><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Date: ${submission.tourDate}</p><p style="font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Time: ${submission.tourTime}</p><p style="font-size: 18px; font-family: sans-serif">I love you mom! You\'re the greatest.</p><p style="font-size: 18px; font-family: sans-serif; font-weight:bold">Tyler</p></div>`,
+    html: `<div><h1 style="font-size: 20px">Here are the details about the tour:</h1><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Name: ${submission.name}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Kid\'s Age: ${submission.childAge}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Date: ${submission.tourDate}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Time: ${submission.tourTime}</p><p style="color: black; font-size: 18px; font-family: sans-serif">I love you mom! You\'re the greatest.</p><p style="color: black; font-size: 18px; font-family: sans-serif; font-weight: bold">Tyler</p></div>`,
   };
 
   return sgMail.send(msgToMom).then(() => {
