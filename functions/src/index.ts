62e389069cc3b8db084ccde4dcab812ad95ab3d2
Tyler Gross <tylergross28@gmail.com>
2018-07-27 00:13:54 -0500

    emails refined

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,12 +10,25 @@ export const tourFormSubmission = functions.firestore.document('tours/{tourId}')
     to: 'tylergross28@gmail.com, tgross1@me.com',
     from: 'support@adventureacademyweb.com',
     subject: 'Hey Mom! Someone Submitted a New Tour Form',
-    html: `<div><h1 style="font-size: 20px">Here are the details about the tour:</h1><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Name: ${submission.name}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Kid\'s Age: ${submission.childAge}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Date: ${submission.tourDate}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Time: ${submission.tourTime}</p><p style="color: black; font-size: 18px; font-family: sans-serif">I love you mom! You\'re the greatest.</p><p style="color: black; font-size: 18px; font-family: sans-serif; font-weight: bold">Tyler</p></div>`,
+    html: `<div style="font-family: sans-serif"><h1 style="font-size: 20px; color: black">Here are the details about the tour:</h1><p style="color: black; font-size: 18px; margin: 3px 10px;">Name: ${submission.name}</p><p style="color: black; font-size: 18px; margin: 3px 10px;">Kid\'s Age: ${submission.childAge}</p><p style="color: black; font-size: 18px; margin: 3px 10px;">Tour Date: ${submission.tourDate}</p><p style="color: black; font-size: 18px; margin: 3px 10px; font-family: sans-serif">Tour Time: ${submission.tourTime}</p><p style="color: black; font-size: 18px;">I love you mom! You\'re the greatest.</p><p style="color: black; font-size: 18px; font-weight: bold">Tyler</p></div>`,
   };
 
-  return sgMail.send(msgToMom).then(() => {
-    console.log('It worked!');
+  const msgToParent = {
+    to: submission.email,
+    from: 'support@adventureacademyweb.com',
+    subject: 'Thank You Scheduling a Tour to See Our Center!',
+    html: `<body style="margin: 0; font-family: sans-serif"><div style="height: 60px; width: 100%; background-color: #49adea"><table><tr><td><img style="height: 40px; width: 40px; margin: 8px" src="https://adventureacademyweb.com/images/white-logo.png"></td><td><h2 style="margin: 0; color: white; font-weight: 300">Adventure Academy</h2></td></tr></table></div><div><h1 style="font-size: 20px; padding-left: 24px;">Thank you for scheduling a tour to see our center!</h1><p style="padding-left: 24px;">Here are the details of your visit:</p><p style="padding-left: 75px; margin: 5px">Tour Day: ${submission.tourDay}</p><p style="padding-left: 75px; margin: 5px">Tour Time: ${submission.tourTime}</p><p style="padding-left: 36px;">We'll see you soon!</p><p style="padding-left: 36px; font-weight: bold; margin: 5px">Lisa Gross</p><p style="padding-left: 36px; margin: 5px">Owner and Director</p><p style="padding-left: 36px; margin: 5px">Adventure Academy Daycare Center</p><p style="padding-left: 36px; margin: 5px">8383 Craig St #210, Indianapolis, IN 46250</p></div></body>`
+  }
+
+  sgMail.send(msgToParent).then(() => {
+    console.log('Message to parent worked.');
   }).catch(error => {
-    console.error('It did not work :[');
+    console.error('Message to parent did not work :[');
+  });
+
+  return sgMail.send(msgToMom).then(() => {
+    console.log('Message to mom worked.');
+  }).catch(error => {
+    console.error('Message to mom did not work :[');
   });
 });
