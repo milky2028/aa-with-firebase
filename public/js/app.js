131fd3a14c6f410d6fdbae62d46c24fdbc006250
Tyler <tylergross28@gmail.com>
2018-08-02 16:10:34 -0500

    linting support

--- public/js/app.js
+++ public/js/app.js
@@ -1,90 +1,89 @@
 const config = {
-    apiKey: "AIzaSyDkRY5dlk-aBvDYpDkyvLLC8nPDxXt2C60",
-    authDomain: "adventure-academ-1501025774496.firebaseapp.com",
-    databaseURL: "https://adventure-academ-1501025774496.firebaseio.com",
-    projectId: "adventure-academ-1501025774496",
-    storageBucket: "adventure-academ-1501025774496.appspot.com",
-    messagingSenderId: "784182344237"
-};
-firebase.initializeApp(config);
-const settings = { timestampsInSnapshots: true };
-const db = firebase.firestore();
-db.settings(settings);
+  apiKey: 'AIzaSyDkRY5dlk-aBvDYpDkyvLLC8nPDxXt2C60',
+  authDomain: 'adventure-academ-1501025774496.firebaseapp.com',
+  databaseURL: 'https://adventure-academ-1501025774496.firebaseio.com',
+  projectId: 'adventure-academ-1501025774496',
+  storageBucket: 'adventure-academ-1501025774496.appspot.com',
+  messagingSenderId: '784182344237'
+}
+firebase.initializeApp(config)
+const settings = { timestampsInSnapshots: true }
+const db = firebase.firestore()
+db.settings(settings)
 
-const convertDate = (date = new Date) => {
-    let month = String(date.getMonth() + 1);
-    let day = String(date.getDate());
-    const year = String(date.getFullYear());
+const convertDate = (date = new Date()) => {
+  let month = String(date.getMonth() + 1)
+  let day = String(date.getDate())
+  const year = String(date.getFullYear())
 
-    if (month.length < 2) { month = '0' + month };
-    if (day.length < 2) { day = '0' + day };
+  if (month.length < 2) { month = '0' + month };
+  if (day.length < 2) { day = '0' + day };
 
-    return `${month}/${day}/${year}`
+  return `${month}/${day}/${year}`
 }
 
 const areFieldsMissing = (objectToCheck) => {
-    let missingFields = false;
-    for (value in objectToCheck) {
-        if (!objectToCheck[value]) {
-            missingFields = true;
-        }
+  let missingFields = false
+  for (const value in objectToCheck) {
+    if (!objectToCheck[value]) {
+      missingFields = true
     }
-    return missingFields;
+  }
+  return missingFields
 }
 
 const onSave = (formNumber, successOrFailNumber) => {
-    const successElement = document.querySelector(`#tourSuccess${successOrFailNumber}`);
-    const errorElement = document.querySelector(`#tourError${successOrFailNumber}`);
-    const name = document.querySelector(`#name${formNumber}`);
-    const email = document.querySelector(`#email${formNumber}`);
-    const age = document.querySelector(`#child-age${formNumber}`);
-    const tourDate = document.querySelector(`#date${formNumber}`);
-    const tourTime = document.querySelector(`#time${formNumber}`);
-    const formData = {
-        name: name.value,
-        email: email.value,
-        childAge: age.value,
-        tourDate: tourDate.value,
-        tourTime: tourTime.value,
-        submitDay: convertDate()
-    };
-    
-    if (!areFieldsMissing(formData)) {
-        db.collection('tours').add(formData).then(docRef => {
-            // error in is happening here. ID's for DOM elements are incorrect
-            errorElement.style.display = 'none';
-            successElement.style.display = 'block';
-            setTimeout(() => {
-                window.location.href = '/thank-you';
-            }, 2500);
-        }).catch(error => console.error('Error posting document:', error))
-    } else {
-        errorElement.style.display = 'block';
-    }
+  const successElement = document.querySelector(`#tourSuccess${successOrFailNumber}`);
+  const errorElement = document.querySelector(`#tourError${successOrFailNumber}`);
+  const name = document.querySelector(`#name${formNumber}`);
+  const email = document.querySelector(`#email${formNumber}`);
+  const age = document.querySelector(`#child-age${formNumber}`);
+  const tourDate = document.querySelector(`#date${formNumber}`);
+  const tourTime = document.querySelector(`#time${formNumber}`);
+  const formData = {
+    name: name.value,
+    email: email.value,
+    childAge: age.value,
+    tourDate: tourDate.value,
+    tourTime: tourTime.value,
+    submitDay: convertDate()
+  }
+
+  if (!areFieldsMissing(formData)) {
+    db.collection('tours').add(formData).then(docRef => {
+      errorElement.style.display = 'none'
+      successElement.style.display = 'block'
+      setTimeout(() => {
+        window.location.href = '/thank-you'
+      }, 2500)
+    }).catch(error => console.error('Error posting document:', error))
+  } else {
+    errorElement.style.display = 'block'
+  }
 }
 
 const onContactSubmit = () => {
-    const successElement = document.querySelector('#contactSuccess');
-    const errorElement = document.querySelector('#contactError');
-    const name = document.querySelector('#nameContact');
-    const email = document.querySelector('#emailContact');
-    const message = document.querySelector('#messageContact');
-    const formData = {
-        name: name.value,
-        email: email.value,
-        message: message.value,
-        submitDay: convertDate()
-    }
+  const successElement = document.querySelector('#contactSuccess')
+  const errorElement = document.querySelector('#contactError')
+  const name = document.querySelector('#nameContact')
+  const email = document.querySelector('#emailContact')
+  const message = document.querySelector('#messageContact')
+  const formData = {
+    name: name.value,
+    email: email.value,
+    message: message.value,
+    submitDay: convertDate()
+  }
 
-    if (!areFieldsMissing(formData)) {
-        db.collection('contactForms').add(formData).then(docRef => {
-            errorElement.style.display = 'none';
-            successElement.style.display = 'block';
-            setTimeout(() => {
-                window.location.href = '/thank-you-contact';
-            }, 2500);
-        }).catch(error => console.error('Error posting document:', error)) 
-    } else {
-        errorElement.style.display = 'block';
-    }
+  if (!areFieldsMissing(formData)) {
+    db.collection('contactForms').add(formData).then(docRef => {
+      errorElement.style.display = 'none'
+      successElement.style.display = 'block'
+      setTimeout(() => {
+        window.location.href = '/thank-you-contact'
+      }, 2500)
+    }).catch(error => console.error('Error posting document:', error))
+  } else {
+    errorElement.style.display = 'block'
+  }
 }
