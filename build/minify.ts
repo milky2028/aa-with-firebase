349974689d7c901af273ffe304defce4f9a215d2
Tyler <tylergross28@outlook.com>
2020-02-25 19:22:41 +0000

    recursively read files

--- build/minify.ts
+++ build/minify.ts
@@ -1,20 +1,35 @@
-import { minify } from 'html-minifier';
+// import { minify } from 'html-minifier';
+import { readdirSync, statSync } from 'fs';
+import path from 'path';
 
-const minifierOptions = {
-  collapseWhitespace: true,
-  removeComments: true,
-  removeOptionalTags: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeTagWhitespace: true,
-  removeAttributeQuotes: true,
-  collapseInlineTagWhitespace: true,
-  removeEmptyAttributes: true,
-  useShortDoctype: true,
-  minifyCSS: true,
-  minifyJS: true
-};
+// const minifierOptions = {
+//   collapseWhitespace: true,
+//   removeComments: true,
+//   removeOptionalTags: true,
+//   removeRedundantAttributes: true,
+//   removeScriptTypeAttributes: true,
+//   removeTagWhitespace: true,
+//   removeAttributeQuotes: true,
+//   collapseInlineTagWhitespace: true,
+//   removeEmptyAttributes: true,
+//   useShortDoctype: true,
+//   minifyCSS: true,
+//   minifyJS: true
+// };
 
-const result = minify('<p>Pizza</p>', minifierOptions);
 
-console.log(result);
+const cwd = process.cwd();
+function recursiveDirRead(dirPath: string): string[] {
+  return readdirSync(dirPath).flatMap((dirItem) => {
+    const dStats = statSync(path.join(dirPath, dirItem))
+    if (dStats.isFile()) {
+      return path.join(dirPath, dirItem);
+    } else {
+      return recursiveDirRead(path.join(dirPath, dirItem));
+    }
+  })
+}
+
+// const result = minify('<p>Pizza</p>', minifierOptions);
+const files = recursiveDirRead(path.join(cwd, 'public'));
+console.log(files);
