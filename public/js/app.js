48af61cded3c47a4215c1bb41351b965d586d8ab
Tyler <tylergross28@gmail.com>
2020-02-26 21:26:23 -0600

    defer my own scripts

--- public/js/app.js
+++ public/js/app.js
@@ -5,95 +5,113 @@ const config = {
   projectId: 'adventure-academ-1501025774496',
   storageBucket: 'adventure-academ-1501025774496.appspot.com',
   messagingSenderId: '784182344237'
+};
+
+window.firebase.initializeApp(config);
+const settings = { timestampsInSnapshots: true };
+const db = window.firebase.firestore();
+db.settings(settings);
+
+function convertDate(date = new Date()) {
+  let month = String(date.getMonth() + 1);
+  let day = String(date.getDate());
+  const year = String(date.getFullYear());
+
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+
+  return `${month}/${day}/${year}`;
 }
 
-window.firebase.initializeApp(config)
-const settings = { timestampsInSnapshots: true }
-const db = window.firebase.firestore()
-db.settings(settings)
-
-const convertDate = (date = new Date()) => {
-  let month = String(date.getMonth() + 1)
-  let day = String(date.getDate())
-  const year = String(date.getFullYear())
-
-  if (month.length < 2) { month = '0' + month };
-  if (day.length < 2) { day = '0' + day };
-
-  return `${month}/${day}/${year}`
-}
-
-const areFieldsMissing = (objectToCheck) => {
-  let missingFields = false
+function areFieldsMissing(objectToCheck) {
+  let missingFields = false;
   for (const value in objectToCheck) {
     if (!objectToCheck[value]) {
-      missingFields = true
+      missingFields = true;
     }
   }
-  return missingFields
+  return missingFields;
 }
 
-const onSave = (formNumber, successOrFailNumber) => {
-  window.event.preventDefault ? window.event.preventDefault() : window.event.returnValue = false
-  const successElement = document.querySelector(`#tourSuccess${successOrFailNumber}`)
-  const errorElement = document.querySelector(`#tourError${successOrFailNumber}`)
-  const name = document.querySelector(`#name${formNumber}`)
-  const email = document.querySelector(`#email${formNumber}`)
-  const age = document.querySelector(`#child-age${formNumber}`)
-  const tourDate = document.querySelector(`#date${formNumber}`)
-  const tourTime = document.querySelector(`#time${formNumber}`)
+function onSave(formNumber, successOrFailNumber) {
+  window.event.preventDefault
+    ? window.event.preventDefault()
+    : (window.event.returnValue = false);
+  const successElement = document.querySelector(
+    `#tourSuccess${successOrFailNumber}`
+  );
+  const errorElement = document.querySelector(
+    `#tourError${successOrFailNumber}`
+  );
+  const fName = document.querySelector(`#name${formNumber}`);
+  const email = document.querySelector(`#email${formNumber}`);
+  const age = document.querySelector(`#child-age${formNumber}`);
+  const tourDate = document.querySelector(`#date${formNumber}`);
+  const tourTime = document.querySelector(`#time${formNumber}`);
   const formData = {
-    name: name.value,
+    name: fName.value,
     email: email.value,
     childAge: age.value,
     tourDate: tourDate.value,
     tourTime: tourTime.value,
     submitDay: convertDate()
-  }
+  };
 
-  const tourDateObject = formData.tourDate ? new Date(formData.tourDate) : null
-  const today = new Date()
-  const validDate = tourDateObject > today
+  const tourDateObject = formData.tourDate ? new Date(formData.tourDate) : null;
+  const today = new Date();
+  const validDate = tourDateObject > today;
 
   if (!areFieldsMissing(formData) && validDate) {
-    db.collection('tours').add(formData).then(docRef => {
-      errorElement.style.display = 'none'
-      successElement.style.display = 'block'
-      setTimeout(() => {
-        window.location.href = '/thank-you'
-      }, 1500)
-    }).catch(error => console.error('Error posting document:', error))
+    db.collection('tours')
+      .add(formData)
+      .then(() => {
+        errorElement.style.display = 'none';
+        successElement.style.display = 'block';
+        setTimeout(() => {
+          window.location.href = '/thank-you';
+        }, 1500);
+      })
+      .catch((error) => console.error('Error posting document:', error));
   } else if (!validDate) {
-    errorElement.style.display = 'block'
-    errorElement.innerHTML = 'Please select a valid date'
+    errorElement.style.display = 'block';
+    errorElement.innerHTML = 'Please select a valid date';
   } else {
-    errorElement.style.display = 'block'
+    errorElement.style.display = 'block';
   }
 }
 
-const onContactSubmit = () => {
-  window.event.preventDefault ? window.event.preventDefault() : window.event.returnValue = false
-  const successElement = document.querySelector('#contactSuccess')
-  const errorElement = document.querySelector('#contactError')
-  const name = document.querySelector('#nameContact')
-  const email = document.querySelector('#emailContact')
-  const message = document.querySelector('#messageContact')
+function onContactSubmit() {
+  window.event.preventDefault
+    ? window.event.preventDefault()
+    : (window.event.returnValue = false);
+  const successElement = document.querySelector('#contactSuccess');
+  const errorElement = document.querySelector('#contactError');
+  const fName = document.querySelector('#nameContact');
+  const email = document.querySelector('#emailContact');
+  const message = document.querySelector('#messageContact');
   const formData = {
-    name: name.value,
+    name: fName.value,
     email: email.value,
     message: message.value,
     submitDay: convertDate()
-  }
+  };
 
   if (!areFieldsMissing(formData)) {
-    db.collection('contactForms').add(formData).then(docRef => {
-      errorElement.style.display = 'none'
-      successElement.style.display = 'block'
-      setTimeout(() => {
-        window.location.href = '/thank-you-contact'
-      }, 1500)
-    }).catch(error => console.error('Error posting document:', error))
+    db.collection('contactForms')
+      .add(formData)
+      .then(() => {
+        errorElement.style.display = 'none';
+        successElement.style.display = 'block';
+        setTimeout(() => {
+          window.location.href = '/thank-you-contact';
+        }, 1500);
+      })
+      .catch((error) => console.error('Error posting document:', error));
   } else {
-    errorElement.style.display = 'block'
+    errorElement.style.display = 'block';
   }
 }
