a0457d1d6d3d7a57d1b676d4b6d9623cffea7523
Tyler <tylergross28@gmail.com>
2020-02-26 21:24:45 -0600

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    "extends": "standard"
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-shadow': ['error', { builtinGlobals: true, hoist: 'all' }],
+    'no-useless-catch': 'error',
+    'arrow-parens': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 };
