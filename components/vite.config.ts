4360aa5aabc246b2289d46a2b535fea3ecdefd08
Tyler Gross <tylergross28@gmail.com>
2025-01-04 17:13:38 -0500

    correctly name chunks

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        entryFileNames: `assets/components.js`,
+        chunkFileNames: `assets/components.js`,
+        assetFileNames: `assets/components.[ext]`,
+      },
+    },
+  },
+});
