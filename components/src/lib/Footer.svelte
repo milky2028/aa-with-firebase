947567ce01917376ef8f875b75b03c1f461d693a
Tyler Gross <tylergross28@gmail.com>
2025-01-04 16:14:41 -0500

    start over

--- components/src/lib/Footer.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts"></script>
-
-<style>
-	footer {
-		background-color: var(--grey);
-	}
-</style>
-
-<footer>This is footer</footer>
--- components/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-import Footer from './Footer.svelte';
-
-const footer = document.querySelector('#svelte-footer') as Element;
-new Footer({ target: footer });
