0af5126b1662ef118afb8d950d8c5a2cfed92768
Tyler Gross <tylergross28@gmail.com>
2025-09-25 17:15:07 -0400

    remove nav for now

--- components/src/main.ts
+++ components/src/main.ts
@@ -1,11 +1,11 @@
 import { mount } from "svelte";
 import "./app.css";
-import Navbar from "./lib/sections/Navbar.svelte";
+// import Navbar from "./lib/sections/Navbar.svelte";
 import GoogleMaps from "./lib/sections/GoogleMaps.svelte";
 import Footer from "./lib/sections/Footer.svelte";
 
-const navbar = document.querySelector("#svelte-navbar")
-if (navbar) mount(Navbar, { target: navbar });
+// const navbar = document.querySelector("#svelte-navbar")
+// if (navbar) mount(Navbar, { target: navbar });
 
 const googleMaps = document.querySelector('#svelte-google-maps')
 if (googleMaps) mount(GoogleMaps, { target: googleMaps })
