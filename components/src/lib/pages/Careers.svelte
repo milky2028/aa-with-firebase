b36b8f438dfe3e28b730060c443b58b42e1969b3
Tyler Gross <tylergross28@gmail.com>
2025-10-19 20:53:47 -0400

    fix width

--- components/src/lib/pages/Careers.svelte
+++ components/src/lib/pages/Careers.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { NAV_HEIGHT } from "../constants/nav-height";
   import Breadcrumbs from "../leaf/Breadcrumbs.svelte";
   import Heading from "../leaf/Heading.svelte";
   import Link from "../leaf/Link.svelte";
@@ -11,28 +12,32 @@
     :global(.header) {
       padding: 2rem 0;
     }
+
+    :global(.clip-width) {
+      max-width: 660px;
+    }
   }
 </style>
 
-<section>
+<section style="margin-top: {NAV_HEIGHT}px">
   <PageContainer>
     <Breadcrumbs />
     <Heading class="header" level={1} openSans>Careers</Heading>
-    <Text>
+    <Text class="clip-width">
       Adventure Academy is always hiring! As a growing center, we are regularly
       seeking new staff. If you love children and want to be a part of their
       journey of growth and exploration, we would love to have you join our
       team!</Text
     >
     <br />
-    <Text
+    <Text class="clip-width"
       >Please send resumes to <Link
         href="mailto:lisa.gross@adventureacademyweb.com?subject=Adventure%20Academy%20Job%20Application"
         orange>lisa.gross@adventureacademyweb.com.</Link
       ></Text
     >
     <br />
-    <Text
+    <Text class="clip-width"
       >All applicants must at least 18 years of age and be willing to undergo a
       background check, tuberculosis test, and drug screening.</Text
     >
